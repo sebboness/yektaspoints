7cebb7555bfeeab55ec9c8a5dde1f554c7305e00
Sebastian Stefaniuk <sebboness@gmail.com>
2024-02-25 17:08:21 -0800

    wip app layout

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
     return (
         <main className="overflow-hidden">
             {/* Top navbar */}
-            <div className="navbar bg-base-100">
+            <div className="navbar ">
                 <div className="flex-1">
-                    <a className="btn btn-ghost text-xl">MyPoints</a>
+                    <a className="btn btn-ghost text-xl">My points</a>
                 </div>
                 <div className="flex-none">
                     <div className="dropdown dropdown-end">
@@ -29,15 +29,37 @@ export default function Home() {
             </div>
         
             <section>
-                <div className="w-screen h-screen grid grid-rows-2 text-white text-4xl xl:grid-cols-2">
-                    {/* Page 1 */}
-                    <div className="w-full h-full bg-blue-800 centered md:h-screen">
-                        <p>Page 1</p>
+                <div className="w-screen grid grid-rows-2 gap-8  text-4xl xl:grid-cols-2">
+                    {/* Left */}
+                    <div className="container mx-auto p-4">
+                        <div className="card bg-base-100 shadow-xl">
+
+
+                            <figure className="px-10 pt-10">
+                                <div className="hero-points-display rounded-xl p-16 bg-lime-400 w-full text-center text-teal-600">
+                                    <div className="text-9xl font-bold">
+                                        213
+                                    </div>
+                                    <div className="text-7xl">
+                                        points
+                                    </div>
+                                </div>
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <p>Wow, great job!</p>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary btn-lg">Request points</button>
+                                    <button className="btn btn-secondary btn-lg">Cashout points</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
-                    {/* Page 2 */}
-                    <div className="w-full h-full bg-black centered md:h-screen">
-                        <p>Page 2</p>
+                    {/* Right */}
+                    <div className="centered">
+                        <div className="container mx-auto border-2 p-4">
+                            <div className="p-2">Right</div>
+                        </div>
                     </div>
                 </div>
             </section>
